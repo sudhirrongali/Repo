24f2da1413daa4be99f0726cd1c99a901befa3cf
sudhirrongali <sudhirrongali@gmail.com>
2023-03-20 00:53:33 +0530

    git work flow v3

--- src/main/java/sdr/demo/GitRepo/GitWorkFlow.java
+++ src/main/java/sdr/demo/GitRepo/GitWorkFlow.java
@@ -14,7 +14,7 @@ public class GitWorkFlow {
 	 */
 	public GitWorkFlow() {
 		// TODO Auto-generated constructor stub
-	}
+	}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
 
 	/**
 	 * @param argss
@@ -22,6 +22,7 @@ public class GitWorkFlow {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stubs
 		System.out.println("Implementing Git changes");
+		System.out.println("checked-in via making changes in origin-configure paths");
 	}
 
 }
