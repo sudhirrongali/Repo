12aa599535bed624acc15113f859132be3e7bddd
sudhirrongali <sudhirrongali@gmail.com>
2023-03-20 00:45:16 +0530

    git work flow v2

--- src/main/java/sdr/demo/GitRepo/GitWorkFlow.java
+++ src/main/java/sdr/demo/GitRepo/GitWorkFlow.java
@@ -20,8 +20,7 @@ public class GitWorkFlow {
 	 * @param argss
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method stubs
 		System.out.println("Implementing Git changes");
 	}
 
