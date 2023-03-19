1152e6dd1823ade983757d5b8bc479eec9b48c56
sudhirrongali <sudhirrongali@gmail.com>
2023-03-20 00:43:37 +0530

    git work flow

---
+++ src/main/java/sdr/demo/GitRepo/GitWorkFlow.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package sdr.demo.GitRepo;
+
+/**
+ * @author Guest1
+ *
+ */
+public class GitWorkFlow {
+
+	/**
+	 * 
+	 */
+	public GitWorkFlow() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param argss
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Implementing Git changes");
+	}
+
+}
