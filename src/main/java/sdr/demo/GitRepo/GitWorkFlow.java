a03003d3e310c08d3d932c752ef7d2f3ce6a59a1
sudhirrongali <saisudhirrongali@gmail.com>
2023-03-25 22:35:22 +0530

    bug fix

--- src/main/java/sdr/demo/GitRepo/GitWorkFlow.java
+++ src/main/java/sdr/demo/GitRepo/GitWorkFlow.java
@@ -22,6 +22,6 @@ public class GitWorkFlow {
 		// TODO Auto-generated method stubs
 		System.out.println("Implementing Git changes");
 		System.out.println("checked-in via making changes in origin-configure paths"); 
-		System.out.println("Open after a week");s
+		System.out.println("Open after a week");
 	}
 }
